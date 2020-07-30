106c3dae12c6f4e60cfa36ee3ecd3361cc33cb52
Brezo <brezo.al@gmail.com>
2020-07-30 10:15:37 +0200

    Validation: Built-in validation rules and validation rules at runtime

--- src/app/customers/customer.component.ts
+++ src/app/customers/customer.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { NgForm } from '@angular/forms';
+import { FormGroup, FormControl, FormBuilder, Validators, FormControlName } from '@angular/forms';
 
 import { Customer } from './customer';
 
@@ -9,15 +9,59 @@ import { Customer } from './customer';
   styleUrls: ['./customer.component.css']
 })
 export class CustomerComponent implements OnInit {
+  customerForm: FormGroup;
   customer = new Customer();
 
-  constructor() { }
+  constructor(private fb: FormBuilder) { }
 
   ngOnInit(): void {
+    this.customerForm = this.fb.group({
+      firstName: ['', [Validators.required, Validators.minLength(3)]], //First element: defailt value, second element: validations rules
+      lastName: ['', [Validators.required, Validators.maxLength(50)]],
+      email: ['', [Validators.required, Validators.email]],
+      phone: '',
+      notification: 'email',
+      sendCatalog: true 
+    })
+
+    // this.customerForm = new FormGroup({
+    //   firstName: new FormControl(),
+    //   lastName: new FormControl(),
+    //   email: new FormControl(),
+    //   sendCatalog: new FormControl(true) //check for send catalog is checked 
+    // });
   }
 
-  save(customerForm: NgForm): void {
-    console.log(customerForm.form);
-    console.log('Saved: ' + JSON.stringify(customerForm.value));
+  setValue(): void { // setValue requires that we set te value of every FormControl in the form model 
+    this.customerForm.setValue({
+      firstName: 'Jack',
+      lastName: 'Harkness',
+      email: 'jack@handsome.com',
+      sendCatalog: false
+    })
+  }
+
+  patchValue(): void { // patchValue doesn't requires that we set te value of every FormControl in the form model 
+    this.customerForm.patchValue({
+      firstName: 'Jack',
+      lastName: 'Harkness'
+    })
+  }
+
+  save(): void {
+    console.log(this.customerForm);
+    console.log('Saved: ' + JSON.stringify(this.customerForm.value));
+  }
+
+  // Change validations on the fly: in this case, if the user sets "phone" the field phone is required. If the user changes to "email", the field phone is not required
+  setNotification(notifyVia: string): void {
+    const phoneControl = this.customerForm.get('phone');
+    if (notifyVia === 'text') {
+      phoneControl.setValidators(Validators.required);
+    } else {
+      phoneControl.clearValidators();
+    }
+    phoneControl.updateValueAndValidity();
   }
 }
+ 
